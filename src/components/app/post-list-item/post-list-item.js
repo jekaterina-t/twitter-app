fad30a0b135a223ff501b73ebf6b5a31da12448e
jekaterina-t <jekaterina.tomiseva@gmail.com>
2021-09-28 23:25:32 +0300

    add all files

--- src/components/app/post-list-item/post-list-item.js
+++ src/components/app/post-list-item/post-list-item.js
@@ -4,32 +4,9 @@ import { Component } from "react";
 import './post-list-item.css';
 
 export default class PostListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        };
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);
-    }
-
-    onImportant() {
-        this.setState(({important}) => ({
-            important: !important
-        }))
-    }
-
-    onLike() {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
 
     render() {
-        const {label} = this.props;
-        const {important, like} = this.state;
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
         let classNames = 'app-list-item d-flex justify-content-between';
 
         if (important) {
@@ -44,19 +21,20 @@ export default class PostListItem extends Component {
             <div className={classNames}>
             <span
             className="app-list-item-label"
-            onClick={this.onLike}>
+            onClick={onToggleLiked}>
                 {label}
             </span>
             <div className="d-flex justify-content-center align-items-center">
                 <button
                     type="button"
                     className="btn-star btn-sm"
-                    onClick={this.onImportant}>
+                    onClick={onToggleImportant}>
                     <i className="fas fa-star"></i>
                 </button>
                 <button
                     type="button"
-                    className="btn-trash btn-sm">
+                    className="btn-trash btn-sm"
+                    onClick={onDelete}>
                     <i className="fa fa-trash-o"></i>
                 </button>
                 <i className="fas fa-heart"></i>
